c225e9e66b2868e762da25685862c9ade9697189
Roope Kallio <5762764+ropelli@users.noreply.github.com>
2025-07-24 09:54:17 +0300

    add go bins to path while deps.sh is running

--- deps.sh
+++ deps.sh
@@ -68,7 +68,7 @@ install_go() {
     sudo rm -rf /usr/local/go
     wget https://go.dev/dl/go1.23.4.linux-amd64.tar.gz -O /tmp/go.tar.gz
     sudo tar -C /usr/local -xzf /tmp/go.tar.gz
-    export PATH="/usr/local/go/bin:$PATH"
+    export PATH="$HOME/go/bin:/usr/local/go/bin:$PATH"
     go version
 }
 
