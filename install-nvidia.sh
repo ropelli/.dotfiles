155c91569996555ec53b9e1a1ba9efd30a44c2eb
Roope Kallio <5762764+ropelli@users.noreply.github.com>
2025-08-12 20:57:28 +0300

    update install-nvidia script.sh

--- install-nvidia.sh
+++ install-nvidia.sh
@@ -1,12 +1,15 @@
+#!/bin/bash
+set -ex
+sudo dnf remove \*nvidia\* --exclude nvidia-gpu-firmware -y
 sudo dnf update -y
-sudo dnf install kernel-devel
-sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda
-modinfo -F version nvidia
+sudo dnf install kernel-devel -y
+sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda -y
+sleep 4m
+sudo modinfo -F version nvidia
 sudo systemctl enable nvidia-hibernate.service nvidia-suspend.service nvidia-resume.service nvidia-powerd.service
 sudo dnf install xorg-x11-drv-nvidia-power
 sudo systemctl enable nvidia-{suspend,resume,hibernate}
 sudo dnf install kmodtool akmods mokutil openssl
-sudo kmodgenca -a
+sudo kmodgenca -a --force
 sudo mokutil --import /etc/pki/akmods/certs/public_key.der
-sleep 5m
 sudo systemctl reboot
